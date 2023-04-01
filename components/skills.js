5559d15dd2d4fb2e5114c0c1114cb83a528cd5dd
John Cosio <johnc@mybizniche.com>
2023-04-01 21:23:22 +0800

    Fixed console warnings

--- components/skills.js
+++ components/skills.js
@@ -6,126 +6,36 @@ const skills = [
         title: 'Front End Development',
         tech: 'Tailwind CSS, Bootstrap, jQuery, Angular, Vue.js, React(This page)',
         years: 11,
-        scores: [{}, {}, {}]
+        scores: [1, 2, 3]
     },
     {
         id: 2,
         title: 'Back End Development',
         tech: 'Laravel, Express.js, Restify, Apigee, Spring Framework, RESTful API, Redis',
         years: 11,
-        scores: [{}, {}, {}, {}]
+        scores: [4, 5, 6, 7]
     },
     {
         id: 3,
         title: 'Database',
         tech: 'PostgreSQL, MySQL/MariaDB, MongoDB',
         years: 11,
-        scores: [{}, {}, {}, {}]
+        scores: [8, 9, 10, 11]
     },
     {
         id: 4,
         title: 'DevOps',
         tech: 'Git, GitHub, Amazon Web Services, Serena Dimensions, Fortify, WebSphere, Vagrant, Docker, Bamboo, Jenkins, Travis CI, Bitbucket Pipelines',
         years: 3,
-        scores: [{}, {}, {}]
+        scores: [12, 13, 14]
     },
     {
         id: 5,
         title: 'Project Management',
         tech: 'Jira, ServiceNow, IBM Notes, Confluence',
         years: 5,
-        scores: [{}, {}, {}]
+        scores: [15, 16, 17]
     }
-]
-
-const positions = [
-    {
-        id: 1,
-        title: 'Back End Developer',
-        department: 'Engineering',
-        closeDate: '2020-01-07',
-        closeDateFull: 'January 7, 2020',
-        applicants: [
-            {
-                name: 'Dries Vincent',
-                email: 'dries.vincent@example.com',
-                imageUrl:
-                    'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            },
-            {
-                name: 'Lindsay Walton',
-                email: 'lindsay.walton@example.com',
-                imageUrl:
-                    'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            },
-            {
-                name: 'Courtney Henry',
-                email: 'courtney.henry@example.com',
-                imageUrl:
-                    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            },
-            {
-                name: 'Tom Cook',
-                email: 'tom.cook@example.com',
-                imageUrl:
-                    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            },
-        ],
-    },
-    {
-        id: 2,
-        title: 'Front End Developer',
-        department: 'Engineering',
-        closeDate: '2020-01-07',
-        closeDateFull: 'January 7, 2020',
-        applicants: [
-            {
-                name: 'Whitney Francis',
-                email: 'whitney.francis@example.com',
-                imageUrl:
-                    'https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            },
-            {
-                name: 'Leonard Krasner',
-                email: 'leonard.krasner@example.com',
-                imageUrl:
-                    'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            },
-            {
-                name: 'Floyd Miles',
-                email: 'floyd.miles@example.com',
-                imageUrl:
-                    'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            },
-        ],
-    },
-    {
-        id: 3,
-        title: 'User Interface Designer',
-        department: 'Design',
-        closeDate: '2020-01-14',
-        closeDateFull: 'January 14, 2020',
-        applicants: [
-            {
-                name: 'Emily Selman',
-                email: 'emily.selman@example.com',
-                imageUrl:
-                    'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            },
-            {
-                name: 'Kristin Watson',
-                email: 'kristin.watson@example.com',
-                imageUrl:
-                    'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            },
-            {
-                name: 'Emma Dorsey',
-                email: 'emma.dorsey@example.com',
-                imageUrl:
-                    'https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            },
-        ],
-    },
 ]
 
 export default function Skills() {
@@ -149,7 +59,8 @@ export default function Skills() {
                                                 <CalendarIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
                                                               aria-hidden="true"/>
                                                 <p>
-                                                    <time dateTime={skill.years}>{skill.years}</time> years
+                                                    <time dateTime={skill.years}>{skill.years}</time>
+                                                    years
                                                 </p>
                                             </div>
                                         </div>
@@ -157,7 +68,8 @@ export default function Skills() {
                                     <div className="mt-4 flex-shrink-0 sm:mt-0 sm:ml-5">
                                         <div className="flex -space-x-1 overflow-hidden">
                                             {skill.scores.map((score) => (
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                <svg key={score} xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                     viewBox="0 0 24 24"
                                                      strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                                     <path strokeLinecap="round" strokeLinejoin="round"
                                                           d="M4.5 12.75l6 6 9-13.5"/>
