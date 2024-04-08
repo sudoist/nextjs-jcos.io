772fd22f6bd58b1f28fa949e2aae7444ebd7460c
John Cosio <johnc@mybizniche.com>
2024-04-08 23:48:44 +0800

    Fix icons in experience not showing

--- components/experience.js
+++ components/experience.js
@@ -9,8 +9,20 @@ import {
 } from '@heroicons/react/20/solid'
 
 const icons = {
-    BuildingOffice2Icon: BuildingOffice2Icon,
-    HomeIcon: HomeIcon,
+    BuildingOffice2Icon: () => (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+             className="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z"/>
+        </svg>
+    ),
+    HomeIcon: () => (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+             className="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819"/>
+        </svg>
+    ),
 };
 
 function classNames(...classes) {
@@ -30,19 +42,16 @@ export default function Experience({experiences}) {
                             ) : null}
                             <div className="relative flex space-x-3">
                                 <div>
-                  <span
-                      className={classNames(
-                          event.iconBackground,
-                          'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white'
-                      )}
-                  >
-                    {React.createElement(icons[event.icon], {className: "h-5 w-5 text-white"})}
-                  </span>
+                                    <span
+                                     className="h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white"
+                                    >
+                                      {React.createElement(icons[event.icon])}
+                                    </span>
                                 </div>
                                 <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
                                     <div>
                                         <p className="font-medium text-gray-900">
-                                            {event.company}{' '}
+                                            {event.company}
                                         </p>
                                         <p className="text-sm text-gray-500">
                                             {event.title}{' '}
